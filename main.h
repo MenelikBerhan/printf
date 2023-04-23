c098f0faef26feb6f3e8242474c99c1dc6e2efc0
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-23 12:58:41 +0100

    Update main header file UPDATE: struct fmt to include an endidx field which stores the end index of each format specifiers UPDATE: printer field type declaration ADD: typedef alias for printer function type ADD: function prototype for int_fmt function

--- main.h
+++ main.h
@@ -11,16 +11,19 @@ typedef struct fmt
 {
   char type[3];
   int width;
+  int endidx;
   bool left;
   int dp;
   bool base_prefix;
   bool i_plus;
   bool p_plus;
   char leading;
-  void (*printer)(void *, struct fmt);
+  char (*printer)(void *, struct fmt *);
 } FMT;
+typedef char (*FMT_FUNC)(void *, struct fmt *);
 
 int _printf(const char *format, ...);
 FMT *get_specifiers(const char *, int *);
+char *int_fmt(void *, FMT);
 
 #endif /* MAIN_H */
