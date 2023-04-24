8407eedd68019c68d892d24cea894469eede116a
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-24 20:03:08 +0300

    Edit prcnt_fmt
    Use (void) instead of __attribute__((unused)) for unused arguments.

--- prcnt_fmt.c
+++ prcnt_fmt.c
@@ -6,23 +6,15 @@
  * @fmt: An FMT type format specifier.
  *
  * Return: The string "%" if fmt->type = "%" or empty string otherwise.
-*/
+ */
 
-char *prcnt_fmt(void *data __attribute__((unused)), FMT *fmt)
+char *prcnt_fmt(void *data, FMT *fmt)
 {
-	char *str;
+	char *str = malloc(sizeof(char) * 2);
+	(void)data;
+	(void)fmt;
 
-	if (fmt->type[0] == '%')
-	{
-		str = malloc(sizeof(char) * 2);
-		str[1] = '\0';
-		str[0] = '%';
-	}
-	else
-	{
-		str = malloc(sizeof(char) * 1);
-		str[0] = '\0';
-	}
-
+	str[0] = '%';
+	str[1] = '\0';
 	return (str);
 }
