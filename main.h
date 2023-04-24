4d82b0bf58a291bde41d880dc8cb6e3cedaa2f82
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-24 02:38:30 +0100

    Add the function prototype for pointer_fmt function to the main header file

--- main.h
+++ main.h
@@ -39,7 +39,8 @@ typedef char (*FMT_FUNC)(void *, FMT *);
 int _printf(const char *format, ...);
 FMT *get_specifiers(const char *, int *);
 char *int_fmt(void *, FMT *);
-char *printable_str(void *data, FMT *fmt);
+char *printable_str(void *, FMT *);
+char *pointer_fmt(void *, FMT *);
 void base_convert(long, int, int, int, int *, char **);
 
 #endif /* MAIN_H */
