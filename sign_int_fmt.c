319107f3d99e79ec8b1d409cbf80cd3925dd7c1e
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-24 23:38:59 +0100

    Add a function that returns an unsigned integer as a string

---
+++ sign_int_fmt.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * sign_int_fmt - converts an integer to a string
+ * @args: variadic argument list
+ * @fmt: specifier details
+ *
+ * Return: the string
+ */
+char *sign_int_fmt(va_list args, FMT *fmt)
+{
+	unsigned long n = va_arg(args, long);
+	int i = 1;
+	char *num = malloc(i + 1);
+
+	base_convert(n, 10, 0, 0, &i, &num);
+	num[i - 1] = '\0';
+	i = p_w_int(i, 0, fmt, &num);
+	if ((fmt->p_plus || fmt->i_plus))
+	{
+		i = (i >= fmt->width) ? i : fmt->width;
+		num = realloc(num, i + 1);
+		if (i > fmt->width || fmt->left)
+			memmove(&num[1], &num[0], fmt->left ? --i : i);
+		num[0] = (fmt->i_plus) ? ' ' : '+';
+	}
+	return (num);
+}
