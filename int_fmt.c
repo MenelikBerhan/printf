8aad9ec49d7dc42e5e54c31057a3d78f1a58ee76
Toby2507 <oluwatobisalau2000@gmail.com>
2023-04-24 11:41:25 +0100

    Update int_fmt function to use a modularized version of the precision and width flag handler

--- int_fmt.c
+++ int_fmt.c
@@ -10,7 +10,7 @@
 char *int_fmt(void *data, FMT *fmt)
 {
 	long n = *(long *)data;
-	int neg = n < 0, i = 1, j, factor;
+	int neg = n < 0, i = 1;
 	char *num = malloc(i + 1);
 
 	if (neg)
@@ -20,24 +20,13 @@ char *int_fmt(void *data, FMT *fmt)
 		i++;
 	}
 	base_convert(n, 10, 0, neg, &i, &num);
-	num[i] = '\0';
-	if (i < fmt->width)
-	{
-		num = realloc(num, (sizeof(char) * (fmt->width + 1)));
-		if (!fmt->left)
-		{
-			factor = fmt->width - i + 1;
-			memmove(&num[neg ? factor + 1 : factor], &num[neg ? 1 : 0], i);
-			memset(neg ? &num[1] : num, fmt->leading, factor);
-		}
-		else
-			for (j = i; j <= fmt->width; j++)
-				num[j - 1] = ' ';
-		num[fmt->width] = '\0';
-	}
+	num[i - 1] = '\0';
+	printf("%s\t", num);
+	i = p_w_int(i, fmt->dp, fmt->width, fmt->left, neg, fmt->leading, &num);
+	printf("%s\n", num);
 	if ((fmt->p_plus || fmt->i_plus) && !neg)
 	{
-		i = (i >= fmt->width) ? i + 1 : fmt->width;
+		i = (i >= fmt->width) ? i : fmt->width;
 		num = realloc(num, i + 1);
 		if (i > fmt->width || fmt->left)
 			memmove(&num[1], &num[0], fmt->left ? --i : i);
