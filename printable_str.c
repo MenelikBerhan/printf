e3393dfb4e980f0568d79581306613818bded750
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-25 23:52:55 +0300

    Update printable_str
    Handle NULL from malloc & va_arg

--- printable_str.c
+++ printable_str.c
@@ -10,11 +10,21 @@
 char *printable_str(va_list args, FMT *fmt)
 {
 	char *s = va_arg(args, char *);
-	int len = _strlen(s);
-	char *str = malloc(len * 4 + 1);
+	int len;
+	char *str;
 	int i, j = 0;
 	(void)fmt;
 
+	if (!s)
+		s = "";
+
+	len = _strlen(s);
+	str = malloc(len * 4 + 1);
+	if (!str)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 	for (i = 0; i < len; i++)
 	{
 		if (s[i] < 32 || s[i] >= 127)
@@ -30,5 +40,6 @@ char *printable_str(va_list args, FMT *fmt)
 		}
 	}
 	str[j] = '\0';
+
 	return (str);
 }
