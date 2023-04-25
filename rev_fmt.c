c6ee215e3b2b1588f72eff147d50e84a30b575ba
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-25 23:08:53 +0300

    Update rev_fmt to handle NULL from malloc & va_arg

--- rev_fmt.c
+++ rev_fmt.c
@@ -15,11 +15,30 @@ char *rev_fmt(va_list args, FMT *fmt)
 	(void)fmt;
 
 	s = va_arg(args, char *);
-	for (i = 0; s[i]; i++)
-		len++;
+
+	if (s)
+	{
+		for (i = 0; s[i]; i++)
+			len++;
+	}
+	else
+		len = 2;
 
 	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
 	str[len] = '\0';
+	if (!s)
+	{
+		/* puts("in if (!s)"); */
+		str[0] = '%';
+		str[1] = 'r';
+		return (str);
+	}
 
 	for (i = 0; i < len; i++)
 		str[i] = s[len - 1 - i];
