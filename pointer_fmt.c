a903f17688c4a85d6d4e1327696dd5d9ba4b2c78
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-29 05:38:49 +0300

    Edit pointer_fmt

--- pointer_fmt.c
+++ pointer_fmt.c
@@ -1,20 +1,52 @@
 #include "main.h"
-
 /**
- * ptr_fmt - sets element values for a String pointed by num.
- * @num: A pointer to a String.
- * @i: length of s element of num, which is a string.
+ * pointer_fmt - converts a pointer address to a string
+ * @args: variadic argument list
+ * @fmt: specifier details
  *
+ * Return: the string
  */
-void ptr_fmt(String *num, int i)
+String pointer_fmt(va_list *args, FMT *fmt)
 {
-	num->s[i] = '\0';
-	i += 2;
-	num->s = realloc(num->s, i + 1);
-	memmove(&num->s[2], &num->s[0], i + 1);
-	num->s[0] = '0';
-	num->s[1] = 'x';
+	String num;
+	long ptr;
+	int i = 1;
+	char nil[] = "(nil)";
+
+	if (fmt->width == -2)
+		fmt->width = va_arg(*args, int);
+	if (fmt->dp == -2)
+		fmt->dp = va_arg(*args, int);
+	ptr = va_arg(*args, long);
+	num.s = malloc(i + 1);
+	if (!num.s)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	if (ptr == -1)
+	{
+		i = 16;
+		num.s = realloc(num.s, i + 1);
+		strcpy(num.s, "ffffffffffffffff");
+	}
+	else
+		base_convert(ptr, 16, 0, 0, &i, &num.s);
+	
+	ptr_fmt(&num, i);
+
+	if (!ptr)
+	{
+		num.s = realloc(num.s, 6);
+		strcpy(num.s, nil);
+		i = 6;
+	}
+	if (i < fmt->width)
+		pointer_width(&num.s, fmt, i);
+	num.len = strlen(num.s);
+	return (num);
 }
+
 /**
  * pointer_width - handles width flag for pointer specifiers
  * @s: buffer holding integer string
@@ -38,6 +70,21 @@ void pointer_width(char **s, FMT *fmt, int i)
 	(*s)[fmt->width] = '\0';
 }
 
+/**
+ * ptr_fmt - sets element values for a String pointed by num.
+ * @num: A pointer to a String.
+ * @i: length of s element of num, which is a string.
+ *
+ */
+void ptr_fmt(String *num, int i)
+{
+	num->s[i] = '\0';
+	i += 2;
+	num->s = realloc(num->s, i + 1);
+	memmove(&num->s[2], &num->s[0], i + 1);
+	num->s[0] = '0';
+	num->s[1] = 'x';
+}
 /**
  * pointer_fmt - converts a pointer address to a string
  * @args: variadic argument list
