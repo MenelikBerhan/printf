d8216853b5b715ac6a69cc049013dec745e9d14a
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-04-29 03:35:28 +0300

    Update pointer_fmt to handle * flag
    Add conditonal to set width and dp.
    Add helper function ptr_fmt to conform to betty .(40 line max)

--- pointer_fmt.c
+++ pointer_fmt.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * ptr_fmt - sets element values for a String pointed by num.
+ * @num: A pointer to a String.
+ * @i: length of s element of num, which is a string.
+ *
+ */
+void ptr_fmt(String *num, int i)
+{
+	num->s[i] = '\0';
+	i += 2;
+	num->s = realloc(num->s, i + 1);
+	memmove(&num->s[2], &num->s[0], i + 1);
+	num->s[0] = '0';
+	num->s[1] = 'x';
+}
+
+/**
  * pointer_fmt - converts a pointer address to a string
  * @args: variadic argument list
  * @fmt: specifier details
@@ -10,10 +26,15 @@
 String pointer_fmt(va_list *args, FMT *fmt)
 {
 	String num;
-	long ptr = va_arg(*args, long);
+	long ptr;
 	int i = 1, factor;
 	char nil[] = "(nil)";
 
+	if (fmt->width == -2)
+		fmt->width = va_arg(*args, int);
+	if (fmt->dp == -2)
+		fmt->dp = va_arg(*args, int);
+	ptr = va_arg(*args, long);
 	num.s = malloc(i + 1);
 	if (!num.s)
 	{
@@ -21,12 +42,7 @@ String pointer_fmt(va_list *args, FMT *fmt)
 		exit(EXIT_FAILURE);
 	}
 	base_convert(ptr, 16, 0, 0, &i, &num.s);
-	num.s[i] = '\0';
-	i += 2;
-	num.s = realloc(num.s, i + 1);
-	memmove(&num.s[2], &num.s[0], i + 1);
-	num.s[0] = '0';
-	num.s[1] = 'x';
+	ptr_fmt(&num, i);
 	if (!ptr)
 	{
 		num.s = realloc(num.s, 6);
