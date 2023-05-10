9f78c79e1e022c99b8bc1de956d3c3d9dffd19c6
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-05-10 08:57:42 +0300

    Edit _printf (clean up required) and null_printer
    Edit null_printer to check for overflow and emulate printf for
    incorrect format directive.
    
    Edit _printf to check for null_printer return, if overflow occurs when
    printing unformatted character.

--- printf.c
+++ printf.c
@@ -13,28 +13,59 @@ void buffer_overflow(String str, char *buffer, int *k, int *l, int *m)
 	int copied = BUFFER_SIZE - *k - 1;
 
 	strncpy(buffer + *k, str.s, copied);
-	*m = (str.len + (*k)) - BUFFER_SIZE;
-	print_buffer(buffer);
+	*m = str.len - copied;
+
+	print_buffer(buffer, BUFFER_SIZE - 1);
 	*k = 0;
 	(*l)++;
+
 	strcpy(buffer, str.s + copied);
 }
 
 /**
  * null_printer - handles exceptions with no printer function
+ * @spe: apointer to FMT type
  * @format: format string
  * @buffer: buffer
  * @i: currrent @format index
  * @k: current @buffer index
+ * @l: number of buffer prints
+ *
+ * Return: 0 if Successul or 1 if failure.
  */
-void null_printer(const char *format, char *buffer, int *i, int *k)
+int null_printer(FMT *spe, const char *format, char *buffer, int *i
+					, int *k, int *l)
 {
-	char exspe[] = "lh";
+	int m = 0;
+	String str;
 
-	if (format[(*i) + 1] == ' ')
-		buffer[(*k)++] = format[(*i) += 2];
+	if (!spe->type)
+	{
+		print_buffer(buffer, *k);
+		return (1);
+	}
+
+	str.len = spe->i_plus ? 2 : 1;
+	str.s = malloc(sizeof(char) * (str.len + 1));
+	if (!str.s)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	str.s[0] = format[*i];
+	str.s[1] = spe->i_plus ? ' ' : '\0';
+
+	if (*k  + str.len > BUFFER_SIZE - 1)
+		buffer_overflow(str, buffer, k, l, &m);
 	else
-		buffer[(*k)++] = format[strchr(exspe, format[(*i) + 1]) ? (*i)++ : (*i)];
+		strncpy(buffer + *k, str.s, str.len);
+
+	*k += m ? m : str.len;
+	*i = spe->endidx - 2;
+
+	free(str.s);
+	return (0);
 }
 
 /**
@@ -56,6 +87,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	specifiers = get_specifiers(format);
 	va_start(args, format);
+
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
@@ -65,24 +97,38 @@ int _printf(const char *format, ...)
 			printer = spe->printer;
 			if (!printer)
 			{
-				null_printer(format, buffer, &i, &k);
+				if (null_printer(spe, format, buffer, &i, &k, &l))
+				{
+					free(specifiers);
+					va_end(args);
+					return (-1);
+				}
 				continue;
 			}
 			str = printer(&args, spe);
-			if (str.len + k >= BUFFER_SIZE - 1)
+
+			if (str.len > BUFFER_SIZE - 1 - k)
 				buffer_overflow(str, buffer, &k, &l, &m);
 			else
-				strcpy(buffer + k, str.s);
-			k += m ? ++m : str.len;
+				strncpy(buffer + k, str.s, str.len);
+			k += m ? m : str.len;
 			i = spe->endidx - 1;
 			free(str.s);
 		}
 		else
+		{
+			if (k >= BUFFER_SIZE - 1)
+			{
+				print_buffer(buffer, BUFFER_SIZE - 1);
+				k = 0;
+				l++;
+			}
 			buffer[k++] = format[i];
+		}
 	}
 	va_end(args);
 	if (k > 0)
-		print_buffer(buffer);
+		print_buffer(buffer, k);
 	free(specifiers);
-	return (l * BUFFER_SIZE + k - l);
+	return (l * (BUFFER_SIZE - 1) + k);
 }
