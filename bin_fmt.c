4fb6de230bcbe3fd5df2a988dbb482f29f3f9e7e
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-05-10 08:17:08 +0300

    Edit bin, hex, oct_fmt after change in p_w_int
    Use return from p_w_int to assign num.len.

--- bin_fmt.c
+++ bin_fmt.c
@@ -18,6 +18,7 @@ String bin_fmt(va_list *args, FMT *fmt)
 		fmt->width = va_arg(*args, int);
 	if (fmt->dp == -2)
 		fmt->dp = va_arg(*args, int);
+
 	n = sign_int_type(args, fmt);
 	num.s = malloc(i + 1);
 	if (!num.s)
@@ -25,9 +26,11 @@ String bin_fmt(va_list *args, FMT *fmt)
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
+
 	base_convert(n, 2, 0, 0, &i, &num.s);
 	num.s[i - 1] = '\0';
+
 	i = p_w_int(i, 0, fmt, &num.s);
-	num.len = strlen(num.s);
+	num.len = i;
 	return (num);
 }
--- hex_fmt.c
+++ hex_fmt.c
@@ -18,6 +18,7 @@ String hex_fmt(va_list *args, FMT *fmt)
 		fmt->width = va_arg(*args, int);
 	if (fmt->dp == -2)
 		fmt->dp = va_arg(*args, int);
+
 	n = sign_int_type(args, fmt);
 	num.s = malloc(i + 1);
 	if (!num.s)
@@ -25,9 +26,12 @@ String hex_fmt(va_list *args, FMT *fmt)
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
+
 	base_convert(n, 16, hex_cap, 0, &i, &num.s);
 	num.s[i - 1] = '\0';
+
 	i = p_w_int(i, 0, fmt, &num.s);
-	num.len = strlen(num.s);
+	num.len = i;
+
 	return (num);
 }
